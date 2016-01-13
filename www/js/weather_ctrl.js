bf69cd62a7a8c2029094126be636b38f429b7a2a
beatface <emma.beaton@gmail.com>
2016-01-13 15:57:13 -0600

    got search history adding to local storage and search history loading in DOM with search click functions

--- www/js/weather_ctrl.js
+++ www/js/weather_ctrl.js
@@ -9,21 +9,46 @@ starter.controller("weatherCtrl", function($scope, $http, $q) {
 	weather.temp = "--";
 	weather.desc = "--";
 
+	function setValues(result) {
+		weather.forecastData = result.data.forecast.simpleforecast.forecastday;
+		weather.temp = result.data.current_observation.temp_f;
+		weather.desc = result.data.current_observation.weather;
+		weather.icon = result.data.current_observation.icon_url;
+		weather.city = result.data.current_observation.display_location.full;
+	}
+
+	function setLocalStorage(result) {
+		var history = JSON.parse(localStorage.getItem('searchHistory')) || {};
+		var city = result.data.current_observation.display_location.full;
+		var id = result.data.current_observation.station_id
+		history[city] = id;
+		console.log("search history is >>>>", history);
+		localStorage.setItem('searchHistory', JSON.stringify(history));
+		weather.searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+	}
+
+
 	//------------//
-	// GET ZIP DATA
+	// GET SEARCH DATA
 	//------------//
-	$scope.getZipWeather = function(event, zip) {
-		console.log("you pressed");
+	$scope.getZipWeather = function(event, search) {
 		if (event.keyCode === 13) {
-			console.log("pressed enter!");
-			$http.get("http://api.wunderground.com/api/c038c875c4755d69/conditions/q/" + zip + ".json")
+			$http.get("http://api.wunderground.com/api/c038c875c4755d69/conditions/forecast/q/" + search + ".json")
 			.then(function(result) {
-				console.log("zip weather >>>>>", result);
-				weather.temp = result.data.current_observation.temp_f;
-				weather.desc = result.data.current_observation.weather;
-				weather.icon = result.data.current_observation.icon_url;
+				console.log("all weather", result);
+				setValues(result);
+				setLocalStorage(result);
 			});
 		}
+	};
+
+	$scope.getOldSearch = function(search) {
+		$http.get("http://api.wunderground.com/api/c038c875c4755d69/conditions/forecast/q/" + search + ".json")
+		.then(function(result) {
+			console.log("all weather", result);
+			setValues(result);
+			setLocalStorage(result);
+		});
 	};
 
 
@@ -41,17 +66,16 @@ starter.controller("weatherCtrl", function($scope, $http, $q) {
 		});
 	})		
 	.then(function(ipdata) {
-		console.log("ipdata", ipdata);
 		var lat = ipdata.location.lat;
 		var long = ipdata.location.lon;
-		$http.get("http://api.wunderground.com/api/c038c875c4755d69/conditions/q/" + lat + "," + long + ".json")
+		$http.get("http://api.wunderground.com/api/c038c875c4755d69/conditions/forecast/q/" + lat + "," + long + ".json")
 		.then(function(result) {
-			console.log("zip weather", result);
-			weather.temp = result.data.current_observation.temp_f;
-			weather.desc = result.data.current_observation.weather;
-			weather.icon = result.data.current_observation.icon_url;
+			setValues(result);
+			setLocalStorage(result);
 		});
 	});; //end q
+
+
 
 
 	//-----------------//
@@ -66,35 +90,15 @@ starter.controller("weatherCtrl", function($scope, $http, $q) {
 		})
 	})		
 	.then(function(geopos) {
-		console.log("geodata", geopos);
 		var lat = geopos.coords.latitude;
 		var long = geopos.coords.longitude;
-		$http.get("http://api.wunderground.com/api/c038c875c4755d69/conditions/q/" + lat + "," + long + ".json")
+		$http.get("http://api.wunderground.com/api/c038c875c4755d69/conditions/forecast/q/" + lat + "," + long + ".json")
 		.then(function(result) {
-			console.log("zip weather", result);
-			weather.temp = result.data.current_observation.temp_f;
-			weather.desc = result.data.current_observation.weather;
-			weather.icon = result.data.current_observation.icon_url;
+			setValues(result);
+			setLocalStorage(result);
 		});
+
 	});; //end q
-
-
-	
-	$scope.showOther = function() {
-		$scope.poop = "POOP";
-	}
-
-	$scope.getLocation = function() {
-	    if (navigator.geolocation) {
-	        navigator.geolocation.getCurrentPosition(function(geopos) {
-	        	console.log("the geopos are ", geopos);
-	        	$scope.lat = geopos.coords.latitude;
-	        	$scope.long = geopos.coords.longitude;
-	        });
-	    } else {
-	        weatherctrl.response = "Geolocation is not supported by this browser.";
-	    }
-	}
 
 
 
