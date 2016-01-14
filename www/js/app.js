c060bd870263ed78317d9971180583e772ecb6d4
beatface <emma.beaton@gmail.com>
2016-01-14 10:16:08 -0600

    fixed indentation

--- www/js/app.js
+++ www/js/app.js
@@ -6,33 +6,33 @@
 var starter = angular.module('starter', ['ionic']);
 
 starter.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	$ionicPlatform.ready(function() {
+		if(window.cordova && window.cordova.plugins.Keyboard) {
+			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+			// for form inputs)
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  })
+			// Don't remove this line unless you know what you are doing. It stops the viewport
+			// from snapping when text inputs are focused. Ionic handles this internally for
+			// a much nicer keyboard experience.
+			cordova.plugins.Keyboard.disableScroll(true);
+		}
+		if(window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	})
 })
 
-  .config(function ($stateProvider, $urlRouterProvider) {
-  $stateProvider
+	.config(function ($stateProvider, $urlRouterProvider) {
+	$stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('root', {
-    url: '/root',
-    templateUrl: 'templates/root.html'
-  });
+	// setup an abstract state for the tabs directive
+	.state('root', {
+		url: '/root',
+		templateUrl: 'templates/root.html'
+	});
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('root');
+	// if none of the above states are matched, use this as the fallback
+	$urlRouterProvider.otherwise('root');
 
 });
